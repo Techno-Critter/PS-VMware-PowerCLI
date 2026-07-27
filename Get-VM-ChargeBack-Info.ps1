a6bba5e71dd39cb563a1a988d23fffc93cfce513
TechnoCritter <34747092+Techno-Critter@users.noreply.github.com>
2026-07-27 13:29:03 -0600

    Update Get-VM-ChargeBack-Info.ps1
    Upgrade to system drive exemption; added comments; cosmetic changes

--- Get-VM-ChargeBack-Info.ps1
+++ Get-VM-ChargeBack-Info.ps1
@@ -32,13 +32,17 @@ $CredentialFileDirectory = 'C:\Temp\Credentials'
 #endregion
 
 #region Cost variables
-#Costs for charge back model used in calculating monthly rate per VM
-# CPU rate is per core
-# RAM is per GB
-# Storage rate is per TB
+<# Costs for charge back model used in calculating monthly rate per VM
+CPU rate is per core
+RAM is per GB
+Storage rate is per TB
+Storage exemption is how much space is allocatted that does not get charged to customer. This number can be 0.
+Storage exemption is per GB.
+#>
 $CPURate = 5.52
 $RAMRate = 1.41
 $StorageRate = 59.4
+$VMStorageExemption = 100
 $FlatRate = 7.55
 # Convert flat rate to currency format
 $FlatCurrency = $FlatRate.ToString("C",[System.Globalization.CultureInfo]::CurrentCulture)
@@ -165,8 +169,17 @@ ForEach($VCServer in $VCServers){
         $VMCounter = 0
         ForEach($VMachine in $VMachines){
             $VMCounter ++
+            $CPUCost = $null
+            $CPUCurrency = $null
+            $RAMCost = $null
+            $RAMCurrency = $null
+            $StorageCost = $null
+            $StorageCurrency = $null
+            $VMTotalHDChargeCapacity = $null
+
             Write-Progress -Activity "vCenter server $VCServer" -Status 'Gathering virtual machine information...' -CurrentOperation $VMachine.Name -PercentComplete ($VMCounter * 100 / ($VMachines | Measure-Object).Count)
 
+            #region VM Hard Disk
             Try{
                 $VMHardDiskProps = Get-HardDisk -VM $VMachine.Name -ErrorAction Stop
             }
@@ -179,7 +192,6 @@ ForEach($VCServer in $VCServers){
                     }) | Out-Null
             }
 
-            #region VM Hard Disk
             $VMTotalHardDiskCapacityRaw = $null
 
             ForEach($VMHardDisk in $VMHardDiskProps){
@@ -193,9 +205,16 @@ ForEach($VCServer in $VCServers){
                 $VMTotalHardDiskCapacityRaw += $VMDKRawCapacity
             }
 
+            $VMStorageExemptionRaw = $VMStorageExemption * 1GB
+            $VMTotalHDChargeCapacity = $VMTotalHardDiskCapacityRaw - $VMStorageExemptionRaw
+            If($VMTotalHDChargeCapacity -lt '0'){
+                $VMTotalHDChargeCapacity = '0'
+            }
+            #endregion
+
             $CPUCost = $VMachine.NumCpu * $CPURate
             $RAMCost = $VMachine.MemoryGB * $RAMRate 
-            $StorageCost = $StorageRate * ($VMTotalHardDiskCapacityRaw  / [double]1TB
+            $StorageCost = $StorageRate * ($VMTotalHDChargeCapacity / [double]1TB)
             $CPUCurrency = $CPUCost.ToString("C",[System.Globalization.CultureInfo]::CurrentCulture)
             $RAMCurrency = $RAMCost.ToString("C",[System.Globalization.CultureInfo]::CurrentCulture)
             $StorageCurrency = $StorageCost.ToString("C",[System.Globalization.CultureInfo]::CurrentCulture)
@@ -203,23 +222,23 @@ ForEach($VCServer in $VCServers){
             $MonthlyCurrency = $MonthlyRate.ToString("C",[System.Globalization.CultureInfo]::CurrentCulture)
 
             $VMData.Add([PSCustomObject]@{ 
-                'Name'         = $VMachine.Name                                 # Column A
-                'CPUs'         = $VMachine.NumCpu                               # Column B
-                'RAM GB'       = $VMachine.MemoryGB                             # Column C
-                'Disks'        = ($VMHardDiskProps | Measure-Object).Count      # Column D
-                'Used Raw'     = $VMachine.UsedSpaceGB * 1GB                    # Column E
-                'Used GB'      = $VMachine.UsedSpaceGB                          # Column F
-                'Capacity Raw' = $VMTotalHardDiskCapacityRaw                    # Column G
-                'Capacity GB'  = ($VMTotalHardDiskCapacityRaw / [double]1GB)    # Column H
-                'Host'         = $VMachine.VMHost.Name                          # Column I
-                'Cluster'      = $VMachine.VMHost.Parent.Name                   # Column J
-                'CPU Cost'     = $CPUCurrency                                   # Column K
-                'RAM Cost'     = $RAMCurrency                                   # Column L
-                'Storage'      = $StorageCurrency                               # Column M
-                'Flat Rate'    = $FlatCurrency                                  # Column N
-                'Monthly Cost' = $MonthlyCurrency                               # Column O
+                'Name'               = $VMachine.Name                               # Column A
+                'CPUs'               = $VMachine.NumCpu                             # Column B
+                'RAM GB'             = $VMachine.MemoryGB                           # Column C
+                'Disks'              = ($VMHardDiskProps | Measure-Object).Count    # Column D
+                'Used Raw'           = $VMachine.UsedSpaceGB * 1GB                  # Column E
+                'Used GB'            = $VMachine.UsedSpaceGB                        # Column F
+                'Capacity Raw'       = $VMTotalHardDiskCapacityRaw                  # Column G
+                'Capacity GB'        = ($VMTotalHardDiskCapacityRaw / [double]1GB)  # Column H
+                'Charge Capacity GB' = ($VMTotalHDChargeCapacity / [double]1GB)     # Column I
+                'Host'               = $VMachine.VMHost.Name                        # Column J
+                'Cluster'            = $VMachine.VMHost.Parent.Name                 # Column K
+                'CPU Cost'           = $CPUCurrency                                 # Column L
+                'RAM Cost'           = $RAMCurrency                                 # Column M
+                'Storage'            = $StorageCurrency                             # Column N
+                'Flat Rate'          = $FlatCurrency                                # Column O
+                'Monthly Cost'       = $MonthlyCurrency                             # Column P
             }) | Out-Null
-            #endregion
         }
     }
     #endregion
@@ -244,20 +263,21 @@ $ExcelProps.Path = $LogFile
 # VM sheet
 $VMDataLastRow = ($VMData | Measure-Object).Count + 1
 If($VMDataLastRow -gt 1){
-    $VMDataHeaderCount = Get-ColumnName ($VMData | Get-Member | Where-Object{$_.MemberType -match 'NoteProperty'} | Measure-Object).Count
-    $VMDataHeaderRow = "'VMs'!`$A`$1:`$$VMDataHeaderCount`$1"
-    $VMCPUsColumn = "'VMs'!`$B`$2:`$B`$$VMDataLastRow"
-    $VMRAMGBColumn = "'VMs'!`$C`$2:`$C`$$VMDataLastRow"
-    $VMDisksColumn = "'VMs'!`$D`$2:`$D`$$VMDataLastRow"
-    $VMUsedRawColumn = "'VMs'!`$E`$2:`$E`$$VMDataLastRow"
-    $VMUsedGBColumn = "'VMs'!`$F`$2:`$F`$$VMDataLastRow"
-    $VMCapacityRawColumn = "'VMs'!`$G`$2:`$G`$$VMDataLastRow"
-    $VMCapacityGBColumn = "'VMs'!`$H`$2:`$H`$$VMDataLastRow"
-    $VMCPUCostColumn = "'VMs'!`$K`$2:`$K`$$VMDataLastRow"
-    $VMRAMCostColumn = "'VMs'!`$L`$2:`$L`$$VMDataLastRow"
-    $VMStorageCostColumn = "'VMs'!`$M`$2:`$M`$$VMDataLastRow"
-    $VMFlatCostColumn = "'VMs'!`$N`$2:`$N`$$VMDataLastRow"
-    $VMMonthlyCostColumn = "'VMs'!`$O`$2:`$O`$$VMDataLastRow"
+    $VMDataHeaderCount        = Get-ColumnName ($VMData | Get-Member | Where-Object{$_.MemberType -match 'NoteProperty'} | Measure-Object).Count
+    $VMDataHeaderRow          = "'VMs'!`$A`$1:`$$VMDataHeaderCount`$1"
+    $VMCPUsColumn             = "'VMs'!`$B`$2:`$B`$$VMDataLastRow"
+    $VMRAMGBColumn            = "'VMs'!`$C`$2:`$C`$$VMDataLastRow"
+    $VMDisksColumn            = "'VMs'!`$D`$2:`$D`$$VMDataLastRow"
+    $VMUsedRawColumn          = "'VMs'!`$E`$2:`$E`$$VMDataLastRow"
+    $VMUsedGBColumn           = "'VMs'!`$F`$2:`$F`$$VMDataLastRow"
+    $VMCapacityRawColumn      = "'VMs'!`$G`$2:`$G`$$VMDataLastRow"
+    $VMCapacityGBColumn       = "'VMs'!`$H`$2:`$H`$$VMDataLastRow"
+    $VMChargeCapacityGBColumn = "'VMs'!`$I`$2:`$I`$$VMDataLastRow"
+    $VMCPUCostColumn          = "'VMs'!`$L`$2:`$L`$$VMDataLastRow"
+    $VMRAMCostColumn          = "'VMs'!`$M`$2:`$M`$$VMDataLastRow"
+    $VMStorageCostColumn      = "'VMs'!`$N`$2:`$N`$$VMDataLastRow"
+    $VMFlatCostColumn         = "'VMs'!`$O`$2:`$O`$$VMDataLastRow"
+    $VMMonthlyCostColumn      = "'VMs'!`$P`$2:`$P`$$VMDataLastRow"
 
     $VMDataStyle = @()
     $VMDataStyle += New-ExcelStyle -Range $VMDataHeaderRow -HorizontalAlignment Center
@@ -268,6 +288,7 @@ If($VMDataLastRow -gt 1){
     $VMDataStyle += New-ExcelStyle -Range $VMUsedGBColumn -NumberFormat '0.00'
     $VMDataStyle += New-ExcelStyle -Range $VMCapacityRawColumn -NumberFormat '0'
     $VMDataStyle += New-ExcelStyle -Range $VMCapacityGBColumn -NumberFormat '0.00'
+    $VMDataStyle += New-ExcelStyle -Range $VMChargeCapacityGBColumn -NumberFormat '0.00'
     $VMDataStyle += New-ExcelStyle -Range $VMCPUCostColumn -NumberFormat 'Currency'
     $VMDataStyle += New-ExcelStyle -Range $VMRAMCostColumn -NumberFormat 'Currency'
     $VMDataStyle += New-ExcelStyle -Range $VMStorageCostColumn -NumberFormat 'Currency'
